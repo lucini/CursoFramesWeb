68867f6fc85d27d2e5f6995608a63bdef4cdc6c6
lucini <rafaellucini@gmail.com>
2017-03-23 22:18:34 -0300

    Teste

---
+++ backend/config/server.js
@@ -0,0 +1,18 @@
+const port = 3004
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const server = express()
+
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+
+server.listen(port, function() {
+  console.log(`BACKEND is running on port ${port}.`)
+})
+
+//server.use(function(req, res, next) {
+//  res.send('Funcionou')
+//})
+
+module.exports = server
